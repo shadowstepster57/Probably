853669aea255618f8b12c74a0e685d1736fc88d5
ImogenOC <heckoftoday@gmail.com>
2013-11-24 21:11:00 -0500

    Imogen's Fire Mage Overhaul
    3 down.

--- rotations/mage/fire.lua
+++ rotations/mage/fire.lua
@@ -1,12 +1,43 @@
 -- SPEC ID 63
 ProbablyEngine.rotation.register(63, {
-    { "Molten Armor", "!player.buff(Molten Armor)" },
-    { "Arcane Brilliance", "!player.buff(Arcane Brilliance)" },
-    { "Combustion",  "target.debuff(Ignite)" },
-    { "Pyroblast", "player.buff(Pyroblast!)" },
-    { "Inferno Blast", "player.buff(Heating Up)" },
-    { "Living Bomb", "target.debuff(Living Bomb).duration <= 3" },
-    { "Scorch", "player.moving" },
-    { "Fireball" }
+
+  -- Cooldowns
+  { "Ice Block", {"modifier.cooldowns", "player.health <= 30"} },
+  { "Cold Snap", {"modifier.cooldowns", "player.health <= 25", "player.spell(Ice Block).cooldown"} },
+  { "Mirror Image", "modifier.cooldowns" },
+  { "Alter Time", "modifier.cooldowns" },
+  { "Presence of Mind", "modifier.cooldowns" },
+  { "Temporal Shield", { "modifier.cooldowns", "player.health <= 80"} },
+  
+    -- Interrupts
+  { "Counterspell", "modifier.interrupts" },
+  { "Frostjaw", "modifier.interrupts" },
+  
+  -- AoE
+  --{ "Blizzard", "modifier.ctrl", "ground" }, -- Broken for the moment.
+  --{ "Flamestrike", "modifier.alt", "ground" }, -- Broken for the moment.
+  --{ "Ring of Frost", "modifier.alt", "ground" }, -- Broken for the moment.
+	
+  -- Mage Bombs
+  { "Nether Tempest", "!target.debuff(Nether Tempest)", "target" }, 
+  { "Nether Tempest", "target.debuff(Nether Tempest).duration <= 2", "target" },
+  { "Living Bomb", "!target.debuff(Living Bomb)", "target" },
+  { "Frost Bomb", "player.spell(Frost Bomb).cooldown = 0", "target" },
+  
+  -- Survivability
+  { "Frost Nova", "target.range <= 9" },
+  { "Blink", "target.range <= 3" },
+  { "Dragon's Breath", "target.range <= 5"},
+  { "Rune of Power", "modifier.shift", "ground" },
+  { "Ice Barrier", "player.health <= 80" },
+  
+  -- Rotation
+  { "Combustion",  "target.debuff(Ignite)" },
+  { "Pyroblast", "player.buff(Pyroblast!)" },
+  { "Inferno Blast", "player.buff(Heating Up)" },
+  { "Living Bomb", "target.debuff(Living Bomb).duration <= 3" },
+  { "Scorch", "player.moving" },
+  { "Fireball" }
   }
-)
+{{ "Arcane Brilliance", "!player.buff" },
+ { "Molten Armor", "!player.buff(Molten Armor)" },})
