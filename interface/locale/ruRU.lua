84d107b7948b67895679149253e5750c2276e50f
Ben Phelps <bphelpsen@gmail.com>
2013-12-17 14:53:22 -0600

    Russian and Spanish languages

---
+++ interface/locale/ruRU.lua
@@ -0,0 +1,92 @@
+-- ProbablyEngine Rotations - https://probablyengine.com/
+-- Released under modified BSD, see attached LICENSE.
+
+ProbablyEngine.locale.new('ruRU')
+
+-- Buttons
+pels('toggle', 'Переключить')
+pels('toggle_tooltip', 'Включить или отключить ротацию.')
+pels('cooldowns', 'Кулдауны')
+pels('cooldowns_tooltip', 'Позволяет использование длинных кулдаунов.')
+pels('multitarget', 'Мульти-таргет')
+pels('multitarget_tooltip', 'Позволяет использование Мульти-таргет абилок.')
+pels('interrupt', 'Прерывания')
+pels('interrupt_tooltip', 'Позволяет прерывать заклинания.')
+pels('drag_to_position', 'Можно Сдвинуть')
+
+-- Buttons Menu
+pels('rtn_default', 'Стандартные Ротации')
+pels('rtn_custom', 'Загруженные Ротации')
+pels('rtn_switch', 'Ротация переключена на: ')
+pels('rtn_nocustom', 'Нет Загруженных Ротаций')
+
+-- General
+pels('left_click', 'Левый клик')
+pels('right_click', 'Правый клик')
+pels('drag', 'Зажмите')
+pels('rotation_loaded', 'ротация загружена!')
+
+-- Minimap
+pels('open_config', 'открывает меню настроек')
+pels('unlock_buttons', 'переключает блокировку фреймов')
+pels('move_minimap', 'что бы сдвинуть кнопку у миникарты')
+
+-- Combat Tracker
+pels('est', 'Est.') -- Estimated
+pels('na', 'n/a') -- not/available
+pels('k', 'k') -- Thousands postfix
+pels('all_units', 'All Units')
+pels('combat_tracker', 'Combat Tracker')
+pels('ttd', 'TTD') -- Time To Death shorthand
+pels('hpr', 'HPR') -- Health Points Remaining shorthand
+
+-- commands
+pels('running_version', 'Вы используете версию')
+pels('build', 'версия')
+pels('help_cycle', 'Ручной цикл ротации.')
+pels('help_toggle', 'Включить/Выключить аддон.')
+pels('help_ct', 'Отобразить трекер боя.')
+pels('help_al', 'Отобразить лог действий.')
+pels('help_version', 'Показать текущую версию.')
+pels('help_help', 'Показать это сообщение.')
+pels('help_turbo', 'Переключение Турбо Мод.')
+pels('unknown_type', 'Неизвестная комманда')
+
+pels('turbo_enable', 'Турбо Мод Включён!')
+pels('turbo_disable', 'Турбо Мод Выключен!')
+
+-- classes
+pels('arcane_mage', 'Аркан Маг')
+pels('fire_mage', 'Фаер Маг')
+pels('frost_mage', 'Фрост Маг')
+pels('holy_paladin', 'Холи Паладин')
+pels('protection_paladin', 'Прото Паладин')
+pels('retribution_paladin', 'Ретри Паладин')
+pels('arms_warrior', 'Армс Вар')
+pels('furry_warrior', 'Фури Вар')
+pels('protection_warrior', 'Прото Вар')
+pels('balance_druid', 'Сова')
+pels('feral_combat_druid', 'Кот')
+pels('guardian_druid', 'Медведь')
+pels('restoration_druid', 'Дерево')
+pels('blood_death_knight', 'Блад ДК')
+pels('frost_death_knight', 'Фрост ДК')
+pels('unholy_death_knight', 'Анхоли ДК')
+pels('beast_mastery_hunter', 'БМ Хант')
+pels('marksmanship_hunter', 'ММ Хант')
+pels('survival_hunter', 'Сурв Хант')
+pels('discipline_priest', 'Дисц Прист')
+pels('holy_priest', 'Холи Прист')
+pels('shadow_priest', 'Шэдоу Прист')
+pels('assassination_rogue', 'Assassination Rogue')
+pels('combat_rogue', 'Комбат Рог')
+pels('subtlety_rogue', 'Саб Рог')
+pels('elemental_shaman', 'Элем Шаман')
+pels('enhancement_shaman', 'Энх Шаман')
+pels('restoration_shaman', 'Рестор Шаман')
+pels('affliction_warlock', 'Аффли Варлок')
+pels('demonology_warlock', 'Демонолог Варлок')
+pels('destruction_warlock', 'Дестро Варлок')
+pels('brewmaster_monk', 'Хмелевар')
+pels('windwalker_monk', 'Танцующий с ветром')
+pels('mistweaver_monk', 'Ткач туманов')
