d20f625728cd7cbcb53ce54685a816a830f6c5ea
Taoko <ask@me.com>
2013-12-19 10:59:56 -0800

    clean up player module

--- system/modules/player.lua
+++ system/modules/player.lua
@@ -1,22 +1,22 @@
 -- ProbablyEngine Rotations - https://probablyengine.com/
 -- Released under modified BSD, see attached LICENSE.
 
-ProbablyEngine.module.register("player", {
-  cast_cache = { },
+local player = {
+  cast_cache = {},
   behind = false,
   behindTime = 0,
   infront = true,
-  infrontTime = 0,
-})
+  infrontTime = 0
+}
 
-ProbablyEngine.module.player.cast = function(spell)
+function player.cast(spell)
   table.insert(ProbablyEngine.module.player.cast_cache, 1, spell)
   if #ProbablyEngine.module.player.cast_cache > 10 then
     table.remove(ProbablyEngine.module.player.cast_cache)
   end
 end
 
-ProbablyEngine.module.player.casted = function(query)
+function player.casted(query)
   local count = 0
   for _, spell in ipairs(ProbablyEngine.module.player.cast_cache) do
     if spell == query then
@@ -27,3 +27,5 @@ ProbablyEngine.module.player.casted = function(query)
   end
   return 0
 end
+
+ProbablyEngine.module.register("player", player)
