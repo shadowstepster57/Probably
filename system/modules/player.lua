df57fba835dc0732bb637be04c224962cbc3b454
Taoko <ask@me.com>
2013-12-19 11:09:05 -0800

    Fix player.spell.casted

--- system/modules/player.lua
+++ system/modules/player.lua
@@ -2,30 +2,29 @@
 -- Released under modified BSD, see attached LICENSE.
 
 local player = {
-  cast_cache = {},
+  castCache = {},
   behind = false,
   behindTime = 0,
   infront = true,
   infrontTime = 0
 }
 
+local nextCastIndex = 1
 function player.cast(spell)
-  table.insert(ProbablyEngine.module.player.cast_cache, 1, spell)
-  if #ProbablyEngine.module.player.cast_cache > 10 then
-    table.remove(ProbablyEngine.module.player.cast_cache)
-  end
+  player.castCache[nextCastIndex] = spell
+  nextCastIndex = nextCastIndex % 10 + 1
 end
 
 function player.casted(query)
   local count = 0
-  for _, spell in ipairs(ProbablyEngine.module.player.cast_cache) do
-    if spell == query then
-        count = count + 1
-    else
-      return count
+
+  for i = 1, 10 do
+    if query == player.castCache[i] then
+      count = count + 1
     end
   end
-  return 0
+
+  return count
 end
 
 ProbablyEngine.module.register("player", player)
