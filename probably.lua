cb9b92314052b9795a59ec10c17f913625bf657f
Taoko <ask@me.com>
2013-12-25 22:28:33 -0800

    Cleanup Debug

--- probably.lua
+++ probably.lua
@@ -1,19 +1,11 @@
 -- ProbablyEngine Rotations - https://probablyengine.com/
 -- Released under modified BSD, see attached LICENSE.
 
--- Lets fire this bitch up
 ProbablyEngine = {
   addonName = "Probably",
-  addonColor = "2277dd",
-  version = "r1479",
-  active = false,
-  full = false,
-  debugging = false,
-  rotations = { },
-  cycleTime = 50,
-  lag = 0
+  addonColor = "2277dd"
 }
 
--- SavedVariables
-ProbablyEngine_ConfigData = { }
-ProbablyEngine_RotationData = { }
+function ProbablyEngine.print(message)
+  print('|cFF'..ProbablyEngine.addonColor..'['..ProbablyEngine.addonName..']|r ' .. message)
+end
--- system/core/debug.lua
+++ system/core/debug.lua
@@ -2,85 +2,59 @@
 -- Released under modified BSD, see attached LICENSE.
 
 ProbablyEngine.debug = {
-  level = {
-    value = 0,
-  },
-  flags = {
-    index = 0,
-  }
+  enabled = false
+}
+local debug = ProbablyEngine.debug
+
+local level = 0
+local flags = {
+  index = 0,
 }
 
-ProbablyEngine.debug.flags.add = function(name)
-  local bitValue = bit.lshift(1, ProbablyEngine.debug.flags.index)
-  ProbablyEngine.debug.flags[name] = bitValue
-  ProbablyEngine.debug.flags.index = ProbablyEngine.debug.flags.index + 1
+local function addFlag(name)
+  local bitValue = bit.lshift(1, flags.index)
+  flags[name] = bitValue
+  flags.index = flags.index + 1
 end
 
-ProbablyEngine.debug.flags.add('library')
-ProbablyEngine.debug.flags.add('listener')
-ProbablyEngine.debug.flags.add('config')
-ProbablyEngine.debug.flags.add('dsl_no_exist')
-ProbablyEngine.debug.flags.add('dsl_call')
-ProbablyEngine.debug.flags.add('dsl_debug')
-ProbablyEngine.debug.flags.add('rotation')
-ProbablyEngine.debug.flags.add('action_block')
-ProbablyEngine.debug.flags.add('queue')
-ProbablyEngine.debug.flags.add('dynamic')
-ProbablyEngine.debug.flags.add('spell_cast')
-ProbablyEngine.debug.flags.add('no_flag')
-ProbablyEngine.debug.flags.add('timer')
-
-ProbablyEngine.debug.flag = function(name)
-  return ProbablyEngine.debug.flags[name]
+local function getFlag(name)
+  return flags[name]
 end
 
-ProbablyEngine.debug.level.add = function(flag)
-  ProbablyEngine.debug.level.value = bit.bor(ProbablyEngine.debug.level.value, ProbablyEngine.debug.flag(flag))
+addFlag('no_flag')
+addFlag('config')
+addFlag('listener')
+addFlag('timer')
+addFlag('library')
+addFlag('dsl_no_exist')
+addFlag('dsl_call')
+addFlag('dsl_debug')
+addFlag('rotation')
+addFlag('action_block')
+addFlag('queue')
+addFlag('dynamic')
+addFlag('spell_cast')
+
+function debug.add(flag)
+  level = bit.bor(level, getFlag(flag))
 end
 
-ProbablyEngine.debug.level.remove = function(flag)
-  ProbablyEngine.debug.level.value = bit.bxor(ProbablyEngine.debug.level.value, ProbablyEngine.debug.flag(flag))
+function debug.remove(flag)
+  level = bit.bxor(level, getFlag(flag))
 end
 
--- defaults
-
-ProbablyEngine.debug.toggle = function()
-  ProbablyEngine.debugging = not ProbablyEngine.debugging
+function debug.toggle()
+  debug.enabled = not debug.enabled
 end
 
-ProbablyEngine.debug.print = function(message, flag)
-  if not ProbablyEngine.debugging then return end
+function debug.print(message, flag)
+  if not debug.enabled then return end
   if flag == nil then flag = 'no_flag' end
-  local bitFlag = ProbablyEngine.debug.flag(flag)
-  if bit.band(bitFlag, ProbablyEngine.debug.level.value) ~= 0 then
+  if bit.band(getFlag(flag), level) ~= 0 then
     print("|cFFff8000[Debug]|r " .. message)
   end
 end
 
-
---[[
-ProbablyEngine.debug = function(message, level)
-  if level == nil then level = 0 end
-  if ProbablyEngine.debugLevel >= level then
-    print("|cFFff8000[Debug]|r " .. message)
-  end
-end]]
-
-ProbablyEngine.print = function(message)
-  print("|cFF"..ProbablyEngine.addonColor.."["..ProbablyEngine.addonName.."] " .. message .. "|r")
-end
-
-ProbablyEngine.dump = function(object) -- protected
-  print(object)
-  ProbablyEngine.have_full = true
-end
-
-ProbablyEngine.full_check = function() -- protected
-  if ProbablyEngine.full == true then return end
-  --RunMacroText("/run ProbablyEngine_full_check_bool = true")
-  if ProbablyEngine_full_check_bool == true then
-    ProbablyEngine.full = true
-    ProbablyEngine_full_check_bool = nil
-    ProbablyEngine.print("full functionality enabled, we are good to go.")
-  end
+function debug.dump(obj)
+  return DevTools_Dump(obj)
 end
