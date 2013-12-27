8cd14bf2d97ca58a6b56a8cf199188a1751abf44
Taoko <ask@me.com>
2013-12-27 14:49:56 -0800

    Don’t care about case sensitivity, fix fly out spells

--- system/core/overloads.lua
+++ system/core/overloads.lua
@@ -42,17 +42,17 @@ function GetSpellName(spell)
 end
 
 function GetSpellBookIndex(spell)
-  local spellName = GetSpellName(spell)
+  local spellName = stringLower(GetSpellName(spell))
 
-  local cache = spellIndexCache[spellName:lower()]
+  local cache = spellIndexCache[spellName]
   if cache then return cache[1], cache[2] end
 
   for t = 1, 2 do
     local _, _, offset, numSpells = GetSpellTabInfo(t)
     local i
     for i = 1, (offset + numSpells) do
-      if GetSpellBookItemName(i, BOOKTYPE_SPELL) == spellName then
-        spellIndexCache[spellName:lower()] = { i, BOOKTYPE_SPELL }
+      if stringLower(GetSpellBookItemName(i, BOOKTYPE_SPELL)) == spellName then
+        spellIndexCache[spellName] = { i, BOOKTYPE_SPELL }
         return i, BOOKTYPE_SPELL
       end
     end
@@ -62,11 +62,12 @@ function GetSpellBookIndex(spell)
   for f = 1, numFlyouts do
     local flyoutID = GetFlyoutID(f)
     local name, description, numSlots, isKnown = GetFlyoutInfo(flyoutID)
-    if isKnown then
+    if isKnown and numSlots > 0 then
       for g = 1, numSlots do
-        local spellID, isKnownSpell = GetFlyoutSlotInfo(flyoutID, g)
-        if isKnownSpell and GetSpellName(spellID) == spellName then
-          spellIndexCache[spellName:lower()] = { spellID, nil }
+        local spellID, _, isKnownSpell = GetFlyoutSlotInfo(flyoutID, g)
+        local name = GetSpellName(spellID)
+        if name and isKnownSpell and stringLower(GetSpellName(spellID)) == spellName then
+          spellIndexCache[spellName] = { spellID, nil }
           return spellID, nil
         end
       end
@@ -76,8 +77,8 @@ function GetSpellBookIndex(spell)
   local numPetSpells = HasPetSpells()
   if numPetSpells then
     for i = 1, numPetSpells do
-      if GetSpellBookItemName(i, BOOKTYPE_PET) == spellName then
-        spellIndexCache[spellName:lower()] = { i, BOOKTYPE_PET }
+      if stringLower(GetSpellBookItemName(i, BOOKTYPE_PET)) == spellName then
+        spellIndexCache[spellName] = { i, BOOKTYPE_PET }
         return i, BOOKTYPE_PET
       end
     end
