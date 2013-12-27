ebbdac4b4ab4bfd6d881799a03f795cd1d152c39
Taoko <ask@me.com>
2013-12-26 16:47:25 -0800

    Cleanup Overloads

--- system/core/overloads.lua
+++ system/core/overloads.lua
@@ -5,66 +5,86 @@ local BOOKTYPE_SPELL = BOOKTYPE_SPELL
 local BOOKTYPE_PET = BOOKTYPE_PET
 local GetNumSpellTabs = GetNumSpellTabs
 local GetSpellBookItemName = GetSpellBookItemName
+local GetSpellInfo = GetSpellInfo
+local GetSpellLink = GetSpellLink
 local GetSpellTabInfo = GetSpellTabInfo
 local HasPetSpells = HasPetSpells
+local stringLower = string.lower
+local stringMatch = string.match
 
--- lets write to the global, how dirty...
+local spellCache = {}
+local itemCache = {}
 
--- since this doesn't exist, make it!
-GetSpellID = function(spell)
-  if type(spell) == "number" then return spell end
-  local match = string.match(GetSpellLink(spell) or '', 'Hspell:(%d+)|h')
-  if match then return tonumber(match) else return false end
+function GetSpellID(spell)
+  if type(spell) == 'number' then return spell end
+
+  local loweredSpell = stringLower(spell)
+  local cache = spellCache[loweredSpell]
+  if cache then return cache end
+
+  local spellID = stringMatch(GetSpellLink(spell) or '', 'Hspell:(%d+)|h')
+  if spellID then
+    spellCache[loweredSpell] = tonumber(spellID)
+    return tonumber(spellID)
+  end
+
+  return false
 end
 
--- this is also useful
-GetSpellName = function(spell)
-  if tonumber(spell) then
-    local spellID = tonumber(spell)
+function GetSpellName(spell)
+  local spellID = tonumber(spell)
+  if spellID then
     return GetSpellInfo(spellID)
   end
+
   return spell
 end
 
-GetSpellBookIndex = function (spell)
-  local spellID = tonumber(spell)
-  if spellID then
-    spell = GetSpellName(spellID)
-  end
+function GetSpellBookIndex(spell)
+  local spellName = GetSpellName(spell)
 
   for t = 1, 2 do
     local _, _, offset, numSpells = GetSpellTabInfo(t)
     local i
     for i = 1, (offset + numSpells) do
-      if GetSpellBookItemName(i, BOOKTYPE_SPELL) == spell then return i, BOOKTYPE_SPELL end
+      if GetSpellBookItemName(i, BOOKTYPE_SPELL) == spellName then return i, BOOKTYPE_SPELL end
     end
   end
 
   local numPetSpells = HasPetSpells()
   if numPetSpells then
     for i = 1, numPetSpells do
-      if GetSpellBookItemName(i, BOOKTYPE_PET) == spell then return i, BOOKTYPE_PET end
+      if GetSpellBookItemName(i, BOOKTYPE_PET) == spellName then return i, BOOKTYPE_PET end
     end
   end
 
-  return nil
+  return false
 end
 
-GetItemID = function(item)
-  if type(item) == "number" then return item end
-  local match = string.match(select(2, GetItemInfo(item)) or '', 'Hitem:(%d+):')
-  if match then return tonumber(match) else return false end
+function GetItemID(item)
+  if type(item) == 'number' then return item end
+
+  local loweredItem = stringLower(item)
+  local cache = itemCache[loweredItem]
+  if cache then return cache end
+
+  local itemID = stringMatch(select(2, GetItemInfo(item)) or '', 'Hitem:(%d+):')
+  if itemID then
+    itemCache[loweredItem] = tonumber(itemID)
+    return tonumber(itemID)
+  end
+
+  return false
 end
 
-UnitId = function(target)
-  return tonumber(UnitGUID(target):sub(6,10), 16)
+function UnitID(target)
+  return tonumber(UnitGUID(target):sub(6, 10), 16)
 end
+UnitId = UnitID
 
-UnitID = UnitId
-
-table.length = function(table)
+function table.length(tbl)
   local count = 0
-  for _,_ in pairs(table) do
+  for _ in pairs(tbl) do
     count = count + 1
   end
   return count
