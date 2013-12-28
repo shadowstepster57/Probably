30a1403796d85c97cc793d22f0ec7e6348f07d2d
Taoko <ask@me.com>
2013-12-28 11:09:34 -0800

    Add callbacks on custom toggles

--- interface/toggle.lua
+++ interface/toggle.lua
@@ -3,7 +3,7 @@
 
 ProbablyEngine.toggle = {}
 
-ProbablyEngine.toggle.create = function(toggle_name, icon, tooltipl1, tooltipl2)
+ProbablyEngine.toggle.create = function(toggle_name, icon, tooltipl1, tooltipl2, callback)
   local toggleCallback = function(self)
     self.checked = not self.checked
     if self.checked then
@@ -12,6 +12,7 @@ ProbablyEngine.toggle.create = function(toggle_name, icon, tooltipl1, tooltipl2)
       ProbablyEngine.buttons.setInactive(toggle_name)
     end
     ProbablyEngine.config.write('button_states', toggle_name, self.checked)
+    if callback then callback(self, mouseButton) end
   end
   ProbablyEngine.buttons.create(toggle_name, icon, toggleCallback, tooltipl1, tooltipl2)
   ProbablyEngine.buttons.loadStates()
