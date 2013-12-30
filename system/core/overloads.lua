1d4a8de358d8f1446e2223e073b755bba75adc8f
Taoko <ask@me.com>
2013-12-30 07:58:43 -0800

    Completely Remove Spell / Item ID Caching

--- system/core/overloads.lua
+++ system/core/overloads.lua
@@ -3,13 +3,11 @@
 
 local BOOKTYPE_SPELL = BOOKTYPE_SPELL
 local BOOKTYPE_PET = BOOKTYPE_PET
-local GetNumSpellTabs = GetNumSpellTabs
 local GetSpellBookItemName = GetSpellBookItemName
 local GetSpellInfo = GetSpellInfo
 local GetSpellLink = GetSpellLink
 local GetSpellTabInfo = GetSpellTabInfo
 local HasPetSpells = HasPetSpells
-local listener = ProbablyEngine.listener
 local stringLower = string.lower
 local stringMatch = string.match
 
@@ -26,13 +24,8 @@ end
 function GetSpellID(spell)
   if type(spell) == 'number' then return spell end
 
-  local loweredSpell = stringLower(spell)
-  local cache = spellCache[loweredSpell]
-  if cache then return cache end
-
   local spellID = stringMatch(GetSpellLink(spell) or '', 'Hspell:(%d+)|h')
   if spellID then
-    spellCache[loweredSpell] = tonumber(spellID)
     return tonumber(spellID)
   end
 
@@ -53,15 +46,11 @@ function GetSpellBookIndex(spell)
   if not spellName then return false end
   spellName = stringLower(spellName)
 
-  local cache = spellIndexCache[spellName]
-  if cache then return cache[1], cache[2] end
-
   for t = 1, 2 do
     local _, _, offset, numSpells = GetSpellTabInfo(t)
     local i
     for i = 1, (offset + numSpells) do
       if stringLower(GetSpellBookItemName(i, BOOKTYPE_SPELL)) == spellName then
-        spellIndexCache[spellName] = { i, BOOKTYPE_SPELL }
         return i, BOOKTYPE_SPELL
       end
     end
@@ -70,13 +59,12 @@ function GetSpellBookIndex(spell)
   local numFlyouts = GetNumFlyouts()
   for f = 1, numFlyouts do
     local flyoutID = GetFlyoutID(f)
-    local name, description, numSlots, isKnown = GetFlyoutInfo(flyoutID)
+    local _, _, numSlots, isKnown = GetFlyoutInfo(flyoutID)
     if isKnown and numSlots > 0 then
       for g = 1, numSlots do
         local spellID, _, isKnownSpell = GetFlyoutSlotInfo(flyoutID, g)
         local name = GetSpellName(spellID)
         if name and isKnownSpell and stringLower(GetSpellName(spellID)) == spellName then
-          spellIndexCache[spellName] = { spellID, nil }
           return spellID, nil
         end
       end
@@ -87,7 +75,6 @@ function GetSpellBookIndex(spell)
   if numPetSpells then
     for i = 1, numPetSpells do
       if stringLower(GetSpellBookItemName(i, BOOKTYPE_PET)) == spellName then
-        spellIndexCache[spellName] = { i, BOOKTYPE_PET }
         return i, BOOKTYPE_PET
       end
     end
@@ -99,13 +86,8 @@ end
 function GetItemID(item)
   if type(item) == 'number' then return item end
 
-  local loweredItem = stringLower(item)
-  local cache = itemCache[loweredItem]
-  if cache then return cache end
-
   local itemID = stringMatch(select(2, GetItemInfo(item)) or '', 'Hitem:(%d+):')
   if itemID then
-    itemCache[loweredItem] = tonumber(itemID)
     return tonumber(itemID)
   end
 
