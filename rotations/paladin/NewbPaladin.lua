565116b605ea4f8a1cc49c8d60a114d4b318e817
ImogenOC <heckoftoday@gmail.com>
2013-12-31 23:55:15 -0500

    NewbPaladin.lua 1-9 Support
    Shift to Hammer of Justice

---
+++ rotations/paladin/NewbPaladin.lua
@@ -0,0 +1,12 @@
+-- Class ID 2 - Paladins Are Sad
+ProbablyEngine.rotation.register(2, {
+
+  -- Keybind
+  { "Hammer of Justice", "modifier.shift" },
+  
+  -- 'Rotation'
+  { "Word of Glory", "player.health < 90" },
+  { "Judgement" },
+  { "Crusader Strike" },
+  }
+)
