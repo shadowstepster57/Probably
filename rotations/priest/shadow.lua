68f998b17426df1f014824160d15758e60489391
ImogenOC <heckoftoday@gmail.com>
2014-01-01 00:40:51 -0500

    Shadow Racials

--- rotations/priest/shadow.lua
+++ rotations/priest/shadow.lua
@@ -2,6 +2,42 @@
 -- 7/11/2013 - Changed by j
 ProbablyEngine.rotation.register(258, {
     
+      -- Racials (Temp)
+  
+  -- =^*~ Alliance ~*^= --
+  -- Dwarves
+  { "Stoneform", "player.health <= 65" },
+ 
+  -- Humans
+  { "Every Man for Himself", "player.state.charm" },
+  { "Every Man for Himself", "player.state.fear" },
+  { "Every Man for Himself", "player.state.incapacitate" },
+  { "Every Man for Himself", "player.state.sleep" },
+  { "Every Man for Himself", "player.state.stun" },
+ 
+  -- Draenei
+  { "Gift of the Naaru", "player.health <= 70", "player" },
+ 
+  -- Gnomes
+  { "Escape Artist", "player.state.root" },
+  { "Escape Artist", "player.state.snare" },
+
+  -- Night Elves
+  { "Shadowmeld", "target.threat >= 80" },
+  { "Shadowmeld", "focus.threat >= 80"},
+  
+  -- <,&~ Horde ~&,> --
+  -- Forsaken
+  { "Will of the Forsaken", "player.state.fear" },
+  { "Will of the Forsaken", "player.state.charm" },
+  { "Will of the Forsaken", "player.state.sleep" },
+  
+  -- Goblins
+  { "Rocket Barrage", "player.moving" },
+  
+  -- _,-=%Pandaren%=-,_ --
+  { "Quaking Palm", "modifier.interrupts" },
+  
     -- Cooldowns
     { "Power Infusion", "modifier.cooldowns" },
     { "Shadowfiend",    "modifier.cooldowns" },
