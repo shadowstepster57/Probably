0ae275d36963d3a8e888272115f5ced90ae1a4da
ImogenOC <heckoftoday@gmail.com>
2014-01-01 00:38:38 -0500

    NewbPaladin Racials

--- rotations/paladin/NewbPaladin.lua
+++ rotations/paladin/NewbPaladin.lua
@@ -1,6 +1,19 @@
 -- Class ID 2 - Paladins Are Sad
 ProbablyEngine.rotation.register(2, {
 
+  -- Racials (Temp)
+  
+  -- =^*~ Alliance ~*^= --
+  -- Dwarves
+  { "Stoneform", "player.health <= 65" },
+ 
+  -- Humans
+  { "Every Man for Himself", "player.state.charm" },
+  { "Every Man for Himself", "player.state.fear" },
+  { "Every Man for Himself", "player.state.incapacitate" },
+  { "Every Man for Himself", "player.state.sleep" },
+  { "Every Man for Himself", "player.state.stun" },
+  
   -- Keybind
   { "Hammer of Justice", "modifier.shift" },
   
