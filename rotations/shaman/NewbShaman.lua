02e664209fff091abfef8a3d99f8247fd72627e8
ImogenOC <heckoftoday@gmail.com>
2014-01-01 00:44:54 -0500

    NewbShaman Racials

--- rotations/shaman/NewbShaman.lua
+++ rotations/shaman/NewbShaman.lua
@@ -1,6 +1,22 @@
 -- Class ID 7 - The Naturalist
 ProbablyEngine.rotation.register(7, {
 
+    -- Racials (Temp)
+  
+  -- =^*~ Alliance ~*^= --
+  -- Dwarves
+  { "Stoneform", "player.health <= 65" },
+
+  -- Draenei
+  { "Gift of the Naaru", "player.health <= 70", "player" },
+ 
+  -- <,&~ Horde ~&,> --
+  -- Goblins
+  { "Rocket Barrage", "player.moving" },
+  
+  -- _,-=%Pandaren%=-,_ --
+  { "Quaking Palm", "modifier.interrupts" },
+  
   -- Buffs
   { "Lightning Shield", "!player.buff(Lightning Shield)" },
 
