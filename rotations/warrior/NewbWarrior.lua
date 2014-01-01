d4608891cee7b92d55902b58b3059158d48a30e0
ImogenOC <heckoftoday@gmail.com>
2014-01-01 00:16:46 -0500

    NewbWarrior.lua 1-9 Support
    Control to Throw
    Shift to Charge

---
+++ rotations/warrior/NewbWarrior.lua
@@ -0,0 +1,15 @@
+-- Class ID 1 - The Cannon Fodder
+ProbablyEngine.rotation.register(1, {
+
+  -- Keybinds
+  { "Throw", "modifier.control" },
+  { "Charge", "modifier.shift" },
+  
+  -- Survival
+  { "Victory Rush"},
+
+  -- Rotation
+  { "Execute" },
+  { "Heroic Strike" },
+  }
+)
