22c8fb435c0a7a4c95a0cfbce1fc355e78908ff6
ImogenOC <heckoftoday@gmail.com>
2014-01-01 00:52:52 -0500

    Racial Accuracy, Derp...

--- rotations/druid/feral.lua
+++ rotations/druid/feral.lua
@@ -2,41 +2,10 @@
 ProbablyEngine.rotation.register(103, {
 
   -- Racials (Temp)
-  
   -- =^*~ Alliance ~*^= --
-  -- Dwarves
-  { "Stoneform", "player.health <= 65" },
- 
-  -- Humans
-  { "Every Man for Himself", "player.state.charm" },
-  { "Every Man for Himself", "player.state.fear" },
-  { "Every Man for Himself", "player.state.incapacitate" },
-  { "Every Man for Himself", "player.state.sleep" },
-  { "Every Man for Himself", "player.state.stun" },
- 
-  -- Draenei
-  { "Gift of the Naaru", "player.health <= 70", "player" },
- 
-  -- Gnomes
-  { "Escape Artist", "player.state.root" },
-  { "Escape Artist", "player.state.snare" },
-
   -- Night Elves
   { "Shadowmeld", "target.threat >= 80" },
   { "Shadowmeld", "focus.threat >= 80"},
-  
-  -- <,&~ Horde ~&,> --
-  -- Forsaken
-  { "Will of the Forsaken", "player.state.fear" },
-  { "Will of the Forsaken", "player.state.charm" },
-  { "Will of the Forsaken", "player.state.sleep" },
-  
-  -- Goblins
-  { "Rocket Barrage", "player.moving" },
-  
-  -- _,-=%Pandaren%=-,_ --
-  { "Quaking Palm", "modifier.interrupts" },
-  
   -- Survival
   { "Renewal", "player.health <= 30" },
   { "Cenarion Ward", "player.health <75" },
