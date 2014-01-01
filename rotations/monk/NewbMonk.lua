eca86eaf06990db2f6dd954e29eb9a7b49c606af
ImogenOC <heckoftoday@gmail.com>
2014-01-01 00:37:27 -0500

    NewbMonk Racials

--- rotations/monk/NewbMonk.lua
+++ rotations/monk/NewbMonk.lua
@@ -1,6 +1,42 @@
 -- Class ID 10 - Four Line Killing Machine
 ProbablyEngine.rotation.register(10, {
 
+  -- Racials (Temp)
+  
+  -- =^*~ Alliance ~*^= --
+  -- Dwarves
+  { "Stoneform", "player.health <= 65" },
+ 
+  -- Humans
+  { "Every Man for Himself", "player.state.charm" },
+  { "Every Man for Himself", "player.state.fear" },
+  { "Every Man for Himself", "player.state.incapacitate" },
+  { "Every Man for Himself", "player.state.sleep" },
+  { "Every Man for Himself", "player.state.stun" },
+ 
+  -- Draenei
+  { "Gift of the Naaru", "player.health <= 70", "player" },
+ 
+  -- Gnomes
+  { "Escape Artist", "player.state.root" },
+  { "Escape Artist", "player.state.snare" },
+
+  -- Night Elves
+  { "Shadowmeld", "target.threat >= 80" },
+  { "Shadowmeld", "focus.threat >= 80"},
+  
+  -- <,&~ Horde ~&,> --
+  -- Forsaken
+  { "Will of the Forsaken", "player.state.fear" },
+  { "Will of the Forsaken", "player.state.charm" },
+  { "Will of the Forsaken", "player.state.sleep" },
+  
+  -- Goblins
+  { "Rocket Barrage", "player.moving" },
+  
+  -- _,-=%Pandaren%=-,_ --
+  { "Quaking Palm", "modifier.interrupts" },
+  
   -- Blink
   { "Roll", "modifier.shift" },
 
