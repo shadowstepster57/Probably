3b85df90e5ee9766906bdb86cb6fa5dfb197fca2
ImogenOC <heckoftoday@gmail.com>
2014-01-01 00:27:17 -0500

    Blood Racials

--- rotations/deathknight/blood.lua
+++ rotations/deathknight/blood.lua
@@ -1,6 +1,38 @@
 -- SPEC ID 250
 ProbablyEngine.rotation.register(250, {
 
+  -- Racials (Temp)
+  
+  -- =^*~ Alliance ~*^= --
+  -- Dwarves
+  { "Stoneform", "player.health <= 65" },
+ 
+  -- Humans
+  { "Every Man for Himself", "player.state.charm" },
+  { "Every Man for Himself", "player.state.fear" },
+  { "Every Man for Himself", "player.state.incapacitate" },
+  { "Every Man for Himself", "player.state.sleep" },
+  { "Every Man for Himself", "player.state.stun" },
+ 
+  -- Draenei
+  { "Gift of the Naaru", "player.health <= 70", "player" },
+ 
+  -- Gnomes
+  { "Escape Artist", "player.state.root" },
+  { "Escape Artist", "player.state.snare" },
+
+  -- <,&~ Horde ~&,> --
+  -- Forsaken
+  { "Will of the Forsaken", "player.state.fear" },
+  { "Will of the Forsaken", "player.state.charm" },
+  { "Will of the Forsaken", "player.state.sleep" },
+  
+  -- Goblins
+  { "Rocket Barrage", "player.moving" },
+  
+  -- _,-=%Pandaren%=-,_ --
+  { "Quaking Palm", "modifier.interrupts" },
+  
   -- defensive
   { "Bone Shield", "!player.buff" },
 
