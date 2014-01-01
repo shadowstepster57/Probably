0e6c176b246c5caff6a28a39e14107b3c11f741d
ImogenOC <heckoftoday@gmail.com>
2013-12-31 23:50:49 -0500

    NewbMonk.lua 1-9 Support
    Shift to Roll

---
+++ rotations/monk/NewbMonk.lua
@@ -0,0 +1,13 @@
+-- Class ID 8 - Table / Portal Machine
+ProbablyEngine.rotation.register(8, {
+
+  -- Blink
+  { "Roll", "modifier.shift" },
+
+  -- Rotation
+  { "Tiger Palm", "player.buff(Tiger Power).duration < 3" },
+  { "Blackout Kick" },
+  { "Jab" },
+  
+  }
+)
