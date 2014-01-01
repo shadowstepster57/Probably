1f9092bdec47eeb6a4e91cf91fbbd8725e7630d3
ImogenOC <heckoftoday@gmail.com>
2014-01-01 00:14:00 -0500

    NewbWarlock.lua 1-9 Support
    Shift for mouseover Corruption

---
+++ rotations/warlock/NewbWarlock.lua
@@ -0,0 +1,14 @@
+-- Class ID 9 - The OP
+ProbablyEngine.rotation.register(9, {
+
+  -- Mouseover
+  { "Corruption", {"modifier.shift", "!mouseover.debuff(Corruption)"}, "mouseover" },
+  
+  -- Survival
+  { "Drain Life", "player.health <= 85" },
+
+  -- Rotation
+  { "Corruption", "!target.debuff(Corruption)" },
+  { "Shadow Bolt" },
+  }
+)
