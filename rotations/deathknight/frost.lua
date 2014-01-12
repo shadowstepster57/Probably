f9886edabb5086c628bbd71baee2e8d2c539d5dc
shadowstepster57 <keenep57@me.com>
2014-01-11 18:38:46 -0600

    fixes to obliterate, frost strike, blood tap
    -Weird rules for OB and Frost Strike, almost like they were backwards.
    -Rime procs had the wrong buff name. The Passive is called "rime" but the buff is called "freezing fog".
    -I also commented out the blood tap logic, as doing it this way will cause hangups if you have blood charges and no depleted runes (doesn't happen much in raids, but constantly when you're going in and out of combat). If you're doing PE you probably want runic empowerment anyway because of the specific situations that Blood Tap requires. If you want to keep it I would come up with a rule that checks to make sure a rune is off cool down.

--- rotations/deathknight/frost.lua
+++ rotations/deathknight/frost.lua
@@ -58,13 +58,13 @@ ProbablyEngine.rotation.register(251, {
   -- w/ Modifier
   { "Pillar of Frost", "modifier.cooldowns" },
   { "Raise Dead", "modifier.cooldowns" },
-  { "Empower Rune Weapon", "modifier.cooldowns", "player.runicpower <= 70", "player.runes(blood).count = 0", "player.runes(unholy).count = 0", "player.runes(frost).count = 0", "player.runes(death).count = 0" },
+  { "Empower Rune Weapon", {"modifier.cooldowns", "player.runicpower <= 70", "player.runes(blood).count = 0", "player.runes(unholy).count = 0", "player.runes(frost).count = 0", "player.runes(death).count = 0"}},
   
   -- Blood Tap Control
-  { "Blood Tap", "player.buff(Blood Charge).count >= 5" },
+  --{ "Blood Tap", "player.buff(Blood Charge).count >= 5" },
 
   -- Disease Control
-  { "Outbreak", "target.debuff(Frost Fever).duration < 3", "target.debuff(Blood Plague).duration <3", "target" },
+  { "Outbreak", {"target.debuff(Frost Fever).duration < 3", "target.debuff(Blood Plague).duration <3"}, "target" },
   { "Howling Blast", "target.debuff(Frost Fever).duration < 3" },
   { "Plague Strike", "target.debuff(Blood Plague).duration < 3" },
   
@@ -72,14 +72,14 @@ ProbablyEngine.rotation.register(251, {
   { "Pestilence", "modifier.multitarget" },
   { "Howling Blast", "modifier.multitarget" },
   { "Frost Strike", "modifier.multitarget" },
-  { "Plague Strike", "modifier.multitarget", "!modifier.last(Plague Strike)", "player.spell(Death and Decay).cooldown" },
+  { "Plague Strike", {"modifier.multitarget", "!modifier.last(Plague Strike)", "player.spell(Death and Decay).cooldown"}},
   
   -- Rotation
-  { "Soul Reaper", "!modifier.multitarget", "target.health < 35" },
-  { "Howling Blast", "!modifier.multitarget", "player.buff(Rime)" },
-  { "Obliterate", "!modifier.multitarget", "player.buff(Killing Machine)" },
-  { "Obliterate", "!modifier.multitarget", "player.runicpower <= 75" },
-  { "Frost Strike", "!modifier.multitarget", "player.runes(frost).count = 0" },
+  { "Soul Reaper", {"!modifier.multitarget", "target.health < 35"}},
+  { "Howling Blast", {"!modifier.multitarget", "player.buff(Freezing Fog)"}},
+  { "Obliterate", {"!modifier.multitarget", "player.buff(Killing Machine)"}},
+  { "Obliterate", {"!modifier.multitarget","player.runicpower <= 75"}},
+  { "Frost Strike", {"!modifier.multitarget", "!player.buff(Killing Machine)"}},
   
   ------------------
   -- End Rotation --
